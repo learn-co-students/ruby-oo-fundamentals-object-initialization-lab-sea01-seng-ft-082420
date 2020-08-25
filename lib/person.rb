66e20b9f53532b0b01df93b2624f2da8896bc1a8
Chanson9892 <chan.hanson5@gmail.com>
2020-08-25 16:16:47 -0700

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+    def initialize(name)
+        @name = name
+    end
+
+    #setter 
+    def name=(person_name)
+      @name = person_name
+    end
+   # getter
+    def name
+      @name
+    end
+end
